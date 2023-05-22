fbb613ec9d687fc0063be430366015c36f187f44
SimoneScordato99 <scordato.simone@yahoo.com>
2023-05-22 16:36:01 +0200

    milestone 2 fatta

---
+++ includes/partials/funzionePass.php
@@ -0,0 +1,32 @@
+<?php
+function randomPass(){
+    $lunghezza = $_GET['lunghezza'];
+    $lettere = $_GET['lettere'];
+    $numeri = $_GET['numeri'];
+    $simboli = $_GET['simboli']; 
+
+    $caratteriDisponibili = '';
+    if( $lettere == true){
+        $mario = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $caratteriDisponibili .= $mario;
+    };
+    if ( $numeri == true){
+        $gabriele = '1234567890';
+        $caratteriDisponibili .= $gabriele;
+    };
+    if ( $simboli == true){
+        $milazzo = "'!#$%&'*+,-./:;<=>?@\^_`{|}~";
+        $caratteriDisponibili .= $milazzo;
+    };
+    
+    $password = '';
+
+    for ($i=0; $i < $lunghezza ; $i++) {         
+        $indiceRandom = rand(0, strlen($caratteriDisponibili));
+        $returnCaratter = $caratteriDisponibili[$indiceRandom];
+        $password .= $returnCaratter;
+    };
+    return $password;
+
+
+};
--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
 
-function randomPass(){
-    $lunghezza = $_GET['lunghezza'];
-    $lettere = $_GET['lettere'];
-    $numeri = $_GET['numeri'];
-    $simboli = $_GET['simboli']; 
-
-    $caratteriDisponibili = '';
-    if( $lettere == true){
-        $mario = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $caratteriDisponibili .= $mario;
-    };
-    if ( $numeri == true){
-        $gabriele = '1234567890';
-        $caratteriDisponibili .= $gabriele;
-    };
-    if ( $simboli == true){
-        $milazzo = "'!#$%&'*+,-./:;<=>?@\^_`{|}~";
-        $caratteriDisponibili .= $milazzo;
-    };
-    
-    $password = '';
-
-    for ($i=0; $i < $lunghezza ; $i++) {         
-        $indiceRandom = rand(0, strlen($caratteriDisponibili));
-        $returnCaratter = $caratteriDisponibili[$indiceRandom];
-        $password .= $returnCaratter;
-    };
-    return $password;
-}
-
-
 ?>
 
 
@@ -40,20 +9,24 @@ function randomPass(){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Password Generator</title>
 </head>
 <body>
-    <form action="" method="GET">
-        <label for="">lunghezza password</label>
-        <input type="number" name="lunghezza" id="">
-        <input type="checkbox" name="lettere" id="">
-        <input type="checkbox" name="numeri" id="">
-        <input type="checkbox" name="simboli" id="">
-        <button type="submit">GENERA</button>
-    </form>
+    <div class="p-4">
+        <form action="./includes/components/index.php" method="GET"  class="d-flex flex-column align-items-start">
+            <label for="">lunghezza password</label>
+            <input class="my-2" type="number" name="lunghezza" id="">
+            <label for="">Lettere</label>
+            <input class="my-2" type="checkbox" name="lettere" id="">
+            <label for="">Numeri</label>
+            <input class="my-2" type="checkbox" name="numeri" id="">
+            <label for="">Simboli</label>
+            <input class="my-2" type="checkbox" name="simboli" id="">
+            <button type="submit">GENERA</button>
+        </form>
+    </div>
 
-    <?php
-    echo randomPass();
-    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
